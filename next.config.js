16c6fd0b92aa87646fb76730f16ab9445424904f
MrR0b0t1x <98mqcwwjc6@privaterelay.appleid.com>
2025-11-08 01:10:01 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    WHOP_CLIENT_ID: process.env.WHOP_CLIENT_ID,
+    WHOP_CLIENT_SECRET: process.env.WHOP_CLIENT_SECRET,
+    MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY,
+    MAILCHIMP_LIST_ID: process.env.MAILCHIMP_LIST_ID,
+  },
+};
